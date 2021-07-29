2f360293cc831f6e54d702bbabed5513d3955f3d
mansur <olaomansur@gmail.com>
2021-07-29 12:53:55 +0100

    express server

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-// const http = require("http");
 const express = require("express");
-
 const server = express();
-
-
-
-// server.listen(8080, () => {
-//   console.log("server is running on http://localhost:8080");
-// });
+const notes = [
+  "I want to sleep",
+  "I want to eat",
+  "I want to make money",
+  "I want to rest",
+  "I want to buy ferarri",
+  "I want to buy shawarma and dash a beggaar",
+];
+server.get("/", (request, response) => {
+  response.send("<h1>Hello Villagers</h1>");
+});
+server.get("/api/notes", (request, response) => {
+  response.send({
+    notes,
+    total: notes.length,
+  });
+});
+server.get("/api/notes/:index", (request, response) => {
+  const index = parseInt(request.params.index);
+  response.send(notes[index]);
+});
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
