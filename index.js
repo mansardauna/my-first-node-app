6f31356573117da610ef84ec7c2cc94eea75793b
mansur <olaomansur@gmail.com>
2021-08-31 11:49:43 +0100

    build

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const server = express();
 server.use(express.json());
+server.use(express.static('build'))
 const morgan = require('morgan');
 
 morgan.token('custom', (req) => {
